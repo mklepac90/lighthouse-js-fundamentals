a61f53b2bf5b72d02806a4ed0182c9f480784a2f
Michael Klepac <mklepac90@gmail.com>
2020-07-23 18:41:56 +0000

    Learning to tell if a number is odd using functions

---
+++ odd.js
@@ -0,0 +1,11 @@
+const isOdd = function(num) {
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(9));
+console.log(isOdd(11));
+console.log(isOdd(21));
+console.log(isOdd(12));
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
