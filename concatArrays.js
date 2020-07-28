d32fcd775c84201ea6e544cd6a11395c3e3001a9
Michael Klepac <mklepac90@gmail.com>
2020-07-28 20:03:01 +0000

    Declaring a function to concatenate two arrays provided as arguments

---
+++ concatArrays.js
@@ -0,0 +1,19 @@
+/*
+Define a function called concat
+The function should, when given two arrays, concatenate the arrays together.
+*/
+
+function concat(array1, array2) {
+  return array1.concat(array2);
+}
+
+
+
+
+
+
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
