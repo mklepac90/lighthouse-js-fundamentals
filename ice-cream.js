0dbc68578a023e7392cf1dd873d865bad9a2fd1d
Michael Klepac <mklepac90@gmail.com>
2020-07-28 14:23:26 +0000

    Scratch pad for LH Labs array exercises on 7-28

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+/*
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+*/
+
+let sum = 3 + 2;
+
+const doubleSum = function(a, b) {
+  let sum = a + b;
+  return sum * 2;
+}
+
+doubleSum(5, 4);
+
+console.log(sum);
+
+
